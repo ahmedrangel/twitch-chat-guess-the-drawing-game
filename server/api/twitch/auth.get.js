23df04ab0476ddeb3eeee6744b50e970c5081feb
Ahmed Rangel <ahmedrangel@outlook.com>
2023-08-03 05:36:06 -0500

    Update auth.get.js

--- server/api/twitch/auth.get.js
+++ server/api/twitch/auth.get.js
@@ -1,13 +1,13 @@
 export default eventHandler(async (event) => {
   const config = useRuntimeConfig(event);
   const { code, error } = getQuery(event);
-
+  const redirectUrl = getRequestURL(event).href;
+  
   if (!code) {
-    // const redirectUrl = getRequestURL(event).href + "/game";
-    return sendRedirect(event, `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${config.twitch.clientId}&redirect_uri=http://localhost:3000/api/twitch/auth&scope=${encodeURIComponent("user:read:email")}`);
+    return sendRedirect(event, `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${config.twitch.clientId}&redirect_uri=${redirectUrl}&scope=${encodeURIComponent("user:read:email")}`);
   }
 
-  const response = await $fetch(`https://id.twitch.tv/oauth2/token?client_id=${config.twitch.clientId}&client_secret=${config.twitch.clientSecret}&code=${code}&grant_type=authorization_code&redirect_uri=http://localhost:3000/api/twitch/auth`, {
+  const response = await $fetch(`https://id.twitch.tv/oauth2/token?client_id=${config.twitch.clientId}&client_secret=${config.twitch.clientSecret}&code=${code}&grant_type=authorization_code&redirect_uri=${redirectUrl}`, {
     method: "POST",
     headers: {
       "Accept": "application/json",
