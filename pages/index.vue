372cc9c5e67018216ba2e1c9b164584ed1e2c8d9
Ahmed Rangel <ahmedrangel@outlook.com>
2023-08-09 06:23:43 -0500

    index fix

--- pages/index.vue
+++ pages/index.vue
@@ -21,16 +21,16 @@ useSeoMeta({
 <template>
   <main id="login">
     <img class="diamond diamond-bg position-absolute top-50 start-50 translate-middle img-fluid" src="/images/diamond-guess-sq.svg">
-    <div class="position-absolute top-50 start-50 translate-middle text-center text-white">
+    <div class="position-absolute top-50 start-50 translate-middle text-center text-white w-100 px-2">
       <div class="mb-4">
-        <h1 class="m-0 drawmond">Drawm<img class="diamond diamond-index img-fluid" src="/images/diamond-guess-sq.svg">nd</h1>
+        <h1 class="mb-1 drawmond">Drawm<img class="diamond diamond-index img-fluid" src="/images/diamond-guess-sq.svg">nd</h1>
         <h5 class="m-0 drawmond-desc text-dark">Challenge and play with your audience to guess your drawings.</h5>
       </div>
-      <a v-if="!loggedIn" class="btn d-flex justify-content-center align-items-center" href="/api/twitch/auth">
+      <a v-if="!loggedIn" class="btn join d-flex justify-content-center align-items-center" href="/api/twitch/auth">
         <Icon class="iconify me-2 h2 m-0" name="simple-icons:twitch" />
         <h2 class="m-0">Join with Twitch</h2>
       </a>
-      <NuxtLink v-else class="btn d-flex justify-content-center align-items-center" to="/game">
+      <NuxtLink v-else class="btn join d-flex justify-content-center align-items-center" to="/game">
         <Icon class="iconify me-2 h2 m-0" name="simple-icons:twitch" />
         <h2 class="m-0">Join with Twitch</h2>
       </NuxtLink>
