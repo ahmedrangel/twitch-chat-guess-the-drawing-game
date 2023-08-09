89de508f1cc563aa13277f59d56e9456f7f783b5
Ahmed Rangel <ahmedrangel@outlook.com>
2023-08-09 07:01:30 -0500

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -79,4 +79,4 @@ export const percentage = (val, total) => {
 export const capitalizeFirstLetter = (string) => {
   string = string.toLowerCase();
   return string.charAt(0).toUpperCase() + string.slice(1);
-}
+};
