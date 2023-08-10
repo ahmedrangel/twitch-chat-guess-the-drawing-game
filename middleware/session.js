79a0a0cbafc519ec74cdfc6185ef781569e5846b
Ahmed Rangel <ahmedrangel@outlook.com>
2023-08-10 03:32:07 -0500

    Update session.js

--- middleware/session.js
+++ middleware/session.js
@@ -1,6 +1,5 @@
 export default defineNuxtRouteMiddleware(() => {
   const { loggedIn } = useUserSession();
-  console.info(loggedIn.value);
   if (!loggedIn.value) {
     return navigateTo("/");
   }
